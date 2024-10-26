3de388a342516f76b651193908c9046a9b8bd230
Nicolen Evanz Aricayos <nicolenevanz.aricayos@gmail.com>
2024-10-26 14:14:26 +0800

    sample game to export apk

--- scripts/Icon.cs
+++ scripts/Icon.cs
@@ -1,16 +1,39 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
 public partial class Icon : Sprite2D
 {
-	// Called when the node enters the scene tree for the first time.
+	private float SPEED = 200.0f;
 	public override void _Ready()
 	{
-		int i = 0;
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		Vector2 position = Position;
+
+		if (Input.IsActionPressed("ui_right"))
+		{
+			position.X += SPEED * (float)delta;
+			RotationDegrees += 1;
+		}
+
+		if (Input.IsActionPressed("ui_left"))
+		{
+			position.X -= SPEED * (float)delta;
+			RotationDegrees += -1;
+		}
+		Position = position;
+	}
+	
+	private void __OnRightPressed(){
+		GD.Print("Right Button Pressed");
+	}
+	
+	private void __OnLeftPressed(){
+		GD.Print("Left Button Pressed");
 	}
 }
