9d3044a022af474654b942a8495a7c49235969cc
Nicolen Evanz Aricayos <nicolenevanz.aricayos@gmail.com>
2024-11-03 21:21:55 +0800

    fixed the physics collision error

--- scripts/DoorArea.cs
+++ scripts/DoorArea.cs
@@ -6,6 +6,7 @@ public partial class DoorArea : Area2D
 	[Signal]
 	public delegate void NextSceneEventHandler();
 	// Called when the node enters the scene tree for the first time.
+	private bool _doNextScene = false;
 	public override void _Ready()
 	{
 	}
@@ -13,12 +14,14 @@ public partial class DoorArea : Area2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (_doNextScene)
+			EmitSignal(SignalName.NextScene);
 	}
 	public void _OnDoorAreaBodyEntered(Node2D body)
 	{
 		if (body is CharacterBody2D character)
 		{
-			EmitSignal(SignalName.NextScene);
+			_doNextScene = true;
 		}
 	}
 }
