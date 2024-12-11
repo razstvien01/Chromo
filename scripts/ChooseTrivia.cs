abb298d92ffb54dcc69c9ec31dafa8b30886b5db
Nicolen Evanz Aricayos <nicolenevanz.aricayos@gmail.com>
2024-12-11 23:47:19 +0800

    added sample UI for the choose trivia and the level

---
+++ scripts/ChooseTrivia.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public partial class ChooseTrivia : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	
+	public void _OnReturnToMainMenuPressed()
+	{
+		QueueFree();
+	}
+	public void _OnLevel1Pressed(){
+		
+	}
+	public void _OnLevel2Pressed(){
+		
+	}
+	public void _OnLevel3Pressed(){
+		
+	}
+	public void _OnLevel4Pressed(){
+		
+	}
+	public void _OnLevel5Pressed(){
+		
+	}
+}
