b6b1b022b60148d7be53308b42945ec550aa8304
Nicolen Evanz Aricayos <nicolenevanz.aricayos@gmail.com>
2024-12-14 23:32:28 +0800

    modified trivia resources

--- scripts/resources/TriviaResource.cs
+++ scripts/resources/TriviaResource.cs
@@ -2,12 +2,13 @@ using Godot;
 
 public partial class TriviaResource : Resource
 {
+    [Export]
+    public int TriviaLevel { get; set; }
+    
     [Export(PropertyHint.MultilineText)]
     public string Trivia { get; set; }
     [Export]
     public string Title { get; set; }
-    [Export]
-    public Texture2D Image { get; set; }
     [Export]
     public AudioStream Narration { get; set; }
     [Export]
